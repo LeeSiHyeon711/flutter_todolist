4022ddcc4da4182fc181606541652f08ef337a6d
Si Hyeon Lee <dltlgus1230@naver.com>
2025-12-16 18:37:06 +0900

    [feat] add_cache

--- flutter_todolist_dev/lib/provider/schedule_provider.dart
+++ flutter_todolist_dev/lib/provider/schedule_provider.dart
@@ -3,6 +3,7 @@ import 'package:TODO_APP_DEV/repository/schedule_repository.dart';
 
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:uuid/uuid.dart';
 
 class ScheduleProvider extends ChangeNotifier {
   final ScheduleRepository repository;  // API 요청 로직을 담은 클래스
@@ -35,23 +36,45 @@ class ScheduleProvider extends ChangeNotifier {
     required ScheduleModel schedule,
   }) async {
     final targetDate = schedule.date;
-    final saveSchedule = await repository.createSchedule(schedule: schedule);
+
+    final uuid = Uuid();
+
+    final tempId = uuid.v4();  // 유일한 ID값을 생성합니다.
+    final newSchedule = schedule.copyWith(
+      id: tempId,  // 임시 ID를 지정합니다.
+    );
 
     cache.update(
       targetDate,
       (value) => [
         ...value,
-        schedule.copyWith(
-          id: saveSchedule,
-        ),
+        newSchedule,
       ]..sort(
         (a, b) => a.startTime.compareTo(
           b.startTime,
         ),
       ),
       // 날짜에 해당되는 값이 없다면 새로운 리스트에 새로운 일정 하나만 추가
-      ifAbsent: () => [schedule],
+      ifAbsent: () => [newSchedule],
     );
+
+    notifyListeners();  // 캐시 업데이트 반영하기
+
+    try {
+      // API 요청을 합니다.
+      final savedSchedule = await repository.createSchedule(schedule: schedule);
+
+      cache.update(
+        targetDate,
+        (value) => value.map((e) => e.id == tempId ? e.copyWith(id: savedSchedule,): e).toList(),
+      );
+    } catch (e) {
+      // 실패 시 임시 ID를 제거하고 캐시 업데이트
+      cache.update(
+        targetDate,
+        (value) => value.where((e) => e.id != tempId).toList(),
+      );
+    }
 
     notifyListeners();
   }
@@ -60,14 +83,28 @@ class ScheduleProvider extends ChangeNotifier {
     required DateTime date,
     required String id,
   }) async {
-    final resp = await repository.deleteSchedule(id: id);
+    final targetSchedule = cache[date]!.firstWhere((e) => e.id == id);  // 삭제할 일정 기억
 
     cache.update(  // 캐시에서 데이터 삭제
       date,
       (value) => value.where((e) => e.id != id).toList(),
       ifAbsent: () => [],
-    );
+    );  // 긍정적 응답(응답 전에 캐시 먼저 업데이트)
 
+    notifyListeners();  // 캐시 업데이트 반영하기
+
+    try {
+      await repository.deleteSchedule(id: id);  // 삭제 함수 실행
+    } catch (e) {
+      // 삭제 실패 시 캐시 롤백하기
+      cache.update(
+        date, (value) => [...value, targetSchedule]..sort(
+          (a, b) => a.startTime.compareTo(
+            b.startTime,
+          ),
+        ),
+      );
+    }
     notifyListeners();
   }
 
